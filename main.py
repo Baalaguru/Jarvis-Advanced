67c1f4bf7458cc1cc7af05a2f06f2ea949199115
Gru555 <schoolbo0y007@gmail.com>
2024-03-05 09:44:44 +0530

    Fix : fixed functionalities

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import subprocess
+
 import pyttsx3
 import speech_recognition as sr
 import pyautogui
 import time
 import os
+import pydub
 import webbrowser
 import sys
 import wikipedia
 import operator
 import datetime
+import cv2
 
-    # engine = pyttsx3.init('sapi5')
-    # voices = engine.getProperty('voices')
-    # engine.setProperty('voice', voices[0].id)
-    # engine.setProperty('rate', 150)
 def speak(audio):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
@@ -68,7 +68,8 @@ if __name__ == "__main__":
         elif 'open youtube' in query:
             speak("what you will like to watch ?")
             qrry = takeCommand().lower()
-            kit.playonyt(f"{qrry}")
+            # kit.playonyt(f"{qrry}")
+            webbrowser.open(f"{qrry}")
         elif 'close chrome' in query:
             os.system("taskkill /f /im chrome.exe")
         elif 'close youtube' in query:
@@ -82,28 +83,26 @@ if __name__ == "__main__":
         elif 'close google' in query:
             os.system("taskkill /f /im msedge.exe")
         elif 'play music' in query:
-            music_dir = 'E:\Musics'
-            songs = os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, random.choice(songs)))
-        elif 'play iron man movie' in query:
-            npath = "E:\ironman.mkv"
-            os.startfile(npath)
-        elif 'close movie' in query:
-            os.system("taskkill /f /im vlc.exe")
-        elif 'close music' in query:
-            os.system("taskkill /f /im vlc.exe")
+            subprocess.call("spotify.exe")
+            # def play_audio(file_path):
+            #    pygame.mixer.init()
+            #    pygame.mixer.music.load("C:\Users\BAALA GURU\Music\A52s5G")
+            #    pygame.mixer.music.play()
+            #    play_audio('audio_file.mp3')
+               # music_dir = '../../Music/A52s5G'
+            # songs = os.listdir(music_dir)
+            # os.startfile(os.path.join(music_dir, random.choice(songs)))
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
-        elif "shut down the system" in query:
-            os.system("shutdown /s /t 5")
+        elif "shut down " in query:
+            os.startfile("shutdown.bat")
         elif "restart the system" in query:
             os.system("shutdown /r /t 5")
-        elif "Lock the system" in query:
+        elif "Lock" in query:
             os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
-        # elif "open notepad" in query:
-            # npath = "C:\WINDOWS\system32\\notepad.exe"
-            # os.startfile(npath)
+        elif "open notepad" in query:
+            os.startfile("C://WINDOWS//system32//notepad.exe")
         elif "close notepad" in query:
             os.system("taskkill /f /im notepad.exe")
         elif "open command prompt" in query:
@@ -114,12 +113,12 @@ if __name__ == "__main__":
             cap = cv2.VideoCapture(0)
             while True:
                 ret, img = cap.read()
-            cv2.imshow('webcam', img)
-            k = cv2.waitKey(50)
-            if k == 27:
-                break;
+                cv2.imshow('webcam', img)
+                k = cv2.waitKey(50)
+                if k == 27:
+                    break;
             cap.release()
-            cv2.destroyAllWndows()
+            cv2.destroyAllWindows()
         elif "go to sleep" in query:
             speak(' alright then, I am switching off')
             sys.exit()
@@ -134,116 +133,30 @@ if __name__ == "__main__":
             r = sr.Recognizer()
             with sr.Microphone() as source:
                 speak("ready")
-            print("Listning...")
-            r.adjust_for_ambient_noise(source)
-            audio = r.listen(source)
-            my_string = r.recognize_google(audio)
-            print(my_string)
-
-
-            def get_operator_fn(op):
-                return {
-                    '+': operator.add,
-                    '-': operator.sub,
-                    'x': operator.mul,
-                    'divided': operator.__truediv__,
-                }[op]
-
-
-            def eval_bianary_expr(op1, oper, op2):
-                op1, op2 = int(op1), int(op2)
-                return get_operator_fn(oper)(op1, op2)
-            speak("your result is")
-            speak(eval_bianary_expr(*(my_string.split())))
-            # elif "what is my ip address" in query:
-            #         speak("Checking")
-            #     try:
-            #         ipAdd = requests.get('https://api.ipify.org').text
-            #     print(ipAdd)
-            #     speak("your ip adress is")
-            #     speak(ipAdd)
-            #     except Exception as e:
-            #     speak("network is weak, please try again some time later")
-            #     elif "volume up" in query:
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #         pyautogui.press("volumeup")
-            #     elif "volume down" in query:
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #         pyautogui.press("volumedown")
-            #     elif "mute" in query:
-            #         pyautogui.press("volumemute")
-            #     elif "refresh" in query:
-            #         pyautogui.moveTo(1551,551, 2)
-            #         pyautogui.click(x=1551, y=551, clicks=1, interval=0, button='right')
-            #         pyautogui.moveTo(1620,667, 1)
-            #         pyautogui.click(x=1620, y=667, clicks=1, interval=0, button='left')
-
-# def perform_action(query):
-#     if 'open chrome' in query:
-#         webbrowser.open("C:\Program Files\Google\Chrome\Application\chrome.exe")
-#     elif 'close chrome' in query:
-#         os.system("taskkill /f /im chrome.exe")
-#     elif 'youtube search' in query:
-#         query = query.replace("youtube search", "")
-#         pyautogui.hotkey('alt', 'd')
-#         time.sleep(1)
-#         pyautogui.press('tab')
-#         pyautogui.press('tab')
-#         pyautogui.press('tab')
-#         pyautogui.press('tab')
-#         time.sleep(1)
-#         pyautogui.write(f"{query}", 0.1)
-#         pyautogui.press('enter')
-#     elif 'open new window' in query:
-#         pyautogui.hotkey('ctrl', 'n')
-#     elif 'open incognito window' in query:
-#         pyautogui.hotkey('ctrl', 'shift', 'n')
-#     elif 'minimise this window' in query:
-#         pyautogui.hotkey('alt', 'space')
-#         time.sleep(1)
-#         pyautogui.press('n')
-#     elif 'open history' in query:
-#         pyautogui.hotkey('ctrl', 'h')
-#     elif 'open downloads' in query:
-#         pyautogui.hotkey('ctrl', 'j')
-#     elif 'previous tab' in query:
-#         pyautogui.hotkey('ctrl', 'shift', 'tab')
-#     elif 'next tab' in query:
-#         pyautogui.hotkey('ctrl', 'tab')
-#     elif 'close tab' in query:
-#         pyautogui.hotkey('ctrl', 'w')
-#     elif 'close window' in query:
-#         pyautogui.hotkey('ctrl', 'shift', 'w')
-#     elif 'clear browsing history' in query:
-#         pyautogui.hotkey('ctrl', 'shift', 'delete')
-#
-# if __name__ == "__main__":
-#     while True:
-#         query = take_command().lower()
-#         perform_action(query)
+                print("Listning...")
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source)
+                my_string = r.recognize_google(audio)
+                print(my_string)
+        elif "search on youtube" in query:
+            webbrowser.open(f"https://www.youtube.com/results?search_query={source}")
+        elif "open steam" in query:
+            subprocess.call("C:\Program Files (x86)\Steam\steam.exe")
+        elif "open obs studio" in query:
+            os.system("../../../../ProgramData/Microsoft/Windows/Start Menu/Programs/OBS Studio/OBS Studio (64bit).lnk")
+        elif "open edge" in query:
+            subprocess.call("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
+        elif "command prompt" in query:
+            subprocess.call("cmd.exe")
+        elif "open chrome" in query:
+            subprocess.call("C:\Program Files\Google\Chrome\Application\chrome.exe")
+        elif "open bleach" in query:
+            subprocess.call("BLEACH Brave Souls - 3D Action.exe")
+        elif "open pc manager" in query:
+            subprocess.call("C:\Program Files\Microsoft PC Manager\MSPCManager.exe")
+        elif "open power toys" in query:
+            subprocess.call("C:\Program Files\PowerToys\PowerToys.exe")
+        elif "open voice" in query:
+            subprocess.call("C:\Program Files\Voice.ai\VoiceAI.exe")
+        elif "let's play" in query:
+            os.startfile("C://Users//BAALA GURU//AppData//Roaming//Microsoft//Windows//Start Menu//Programs//Steam//BLEACH Brave Souls - 3D Action.url")
